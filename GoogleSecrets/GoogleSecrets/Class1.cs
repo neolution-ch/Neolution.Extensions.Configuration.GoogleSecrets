55109395a28032bebe9b73278ca5c49def8ad7e8
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2021-10-25 11:38:56 +0200

    push

--- GoogleSecrets/GoogleSecrets/Class1.cs
+++ GoogleSecrets/GoogleSecrets/Class1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace GoogleSecrets
+namespace Neolution.Extensions.Configuration.GoogleSecrets
 {
-    public class Class1
+    class Class1
     {
     }
 }
