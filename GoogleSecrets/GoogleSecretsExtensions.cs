1e1520f3f66d234c46088ef5302bbc1931d965da
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2021-10-25 17:37:54 +0200

    push

---
+++ GoogleSecrets/GoogleSecretsExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Neolution.Extensions.Configuration.GoogleSecrets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// The Google Secrets Extensions
+    /// </summary>
+    public static class GoogleSecretsExtensions
+    {
+        /// <summary>
+        /// Adds the google secrets.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="options">The options.</param>
+        /// <returns>The IConfiguration Builder</returns>
+        /// <exception cref="System.ArgumentNullException">options</exception>
+        public static IConfigurationBuilder AddGoogleSecrets(this IConfigurationBuilder configuration, Action<GoogleSecretsOptions> options)
+        {
+            _ = options ?? throw new ArgumentNullException(nameof(options));
+            _ = configuration ?? throw new ArgumentNullException(nameof(options));
+
+            var googleSecretsOptions = new GoogleSecretsOptions();
+            options(googleSecretsOptions);
+            configuration.Add(new GoogleSecretsSource(googleSecretsOptions));
+
+            return configuration;
+        }
+    }
+}
